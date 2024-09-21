ebad50dc0873c58376a3867815d0084130e14c54
21270toba <21270@toba-cmt.ac.jp>
2024-09-22 06:27:14 +0900

    Update common.php
    個人情報保護

--- common.php
+++ common.php
@@ -4,8 +4,8 @@ date_default_timezone_set('Asia/Tokyo'); // タイムゾーンを設定
 
 define("DB_SERVER", "localhost"); // サーバ名
 define("DB_DATABASE", "map_app"); // データベース名
-define("DB_USERNAME", "root");    // データベースのユーザ名
-define("DB_PASSWORD", "ここには自分のパスワードを書く"); // データベースのパスワード
+define("DB_USERNAME", "非公開");    // データベースのユーザ名
+define("DB_PASSWORD", "非公開"); // データベースのパスワード
 define("PDO_DSN", "mysql:host=" . DB_SERVER . ";dbname=" . DB_DATABASE . ";charset=utf8;");
 
 
